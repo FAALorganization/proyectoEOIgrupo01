f92aa0d3add5ea8c659474bc5568b7dbff69ff9e
Angeltorresrepo <angel.torrmoz@gmail.com>
2025-04-07 12:20:37 +0200

    Adición de las pantallas Vacaciones, Ausencias y calendario.
    Añadido nuevas funcionalidades de Responsable.

--- src/main/java/com/grupo01/java6/faal/controllers/DefaultController.java
+++ src/main/java/com/grupo01/java6/faal/controllers/DefaultController.java
@@ -76,6 +76,41 @@ public class DefaultController {
         return "calendario"; // View name
     }
 
+    @GetMapping("/gestionVacaciones")
+    public String showgestionVacaciones()
+    {
+
+        return "gestionVacaciones"; // View name
+    }
+
+    @GetMapping("/gestionAusencias")
+    public String showgestionAusencias()
+    {
+
+        return "gestionAusencias"; // View name
+    }
+
+    @GetMapping("/gestionARes")
+    public String showgestionARes()
+    {
+
+        return "gestionARes"; // View name
+    }
+
+    @GetMapping("/gestionVRes")
+    public String showgestionVRes()
+    {
+
+        return "gestionVRes"; // View name
+    }
+
+    @GetMapping("/creacionTicket")
+    public String showcreacionTicket()
+    {
+
+        return "creacionTicket"; // View name
+    }
+
 
     @GetMapping("/login2")
     public String showLogin()
@@ -85,7 +120,7 @@ public class DefaultController {
     }
 
     @GetMapping("/tareas")
-    public String ListTask()
+    public String listTask()
     {
 
         return "tareas"; // View name
