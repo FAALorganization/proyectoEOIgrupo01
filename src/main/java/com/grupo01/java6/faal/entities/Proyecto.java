9b72f0e3ce7f07e14e742c10bd1a0ff5cb8bb83a
EOIGrupo1 <eoigrupo01@gmail.com>
2025-05-27 03:34:13 +0200

    Revert "Scrum 37 thymeleaf html"

---
+++ src/main/java/com/grupo01/java6/faal/entities/Proyecto.java
@@ -0,0 +1,35 @@
+package com.grupo01.java6.faal.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "proyecto")
+public class Proyecto implements java.io.Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idProyecto", nullable = false)
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "fechaInicio")
+    private LocalDate fechaInicio;
+
+    @Column(name = "fechaFin")
+    private LocalDate fechaFin;
+
+
+
+}
