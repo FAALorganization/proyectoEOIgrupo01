143440387945337a13c74f9659c1e8792a7facd5
Angeltorresrepo <angel.torrmoz@gmail.com>
2025-06-11 21:13:35 +0200

    Termino SEcurity config + Data loader

--- src/main/java/com/grupo01/java6/faal/config/UserDetailsImpl.java
+++ src/main/java/com/grupo01/java6/faal/config/UserDetailsImpl.java
@@ -1,21 +1,29 @@
 package com.grupo01.java6.faal.config;
 
 import com.grupo01.java6.faal.entities.Login;
+import com.grupo01.java6.faal.entities.Roles;
+import com.grupo01.java6.faal.repositories.LoginRepository;
 import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 
 public class UserDetailsImpl implements UserDetails, CredentialsContainer {
 
     private final Login login;
+    private final LoginRepository loginRepository;
 
-    public UserDetailsImpl(Login login) {
+    public UserDetailsImpl(Login login, LoginRepository loginRepository) {
         this.login = login;
+        this.loginRepository = loginRepository;
     }
 
     @Override
@@ -63,5 +71,24 @@ public class UserDetailsImpl implements UserDetails, CredentialsContainer {
     public void eraseCredentials() {
 
     }
+
+//    @Override //porqué sale como override??
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//
+//        System.out.println("loadUserByUsername email : " + email);
+//        Login  usuario = loginRepository.findUsuarioByEmailAndActiveTrue(email); //hay que creare este metodo
+//        System.out.println("loadUserByUsername usuario : " + usuario.getIdDetallesDeUsuario().getNombre());
+//        org.springframework.security.core.userdetails.User springUser = null;
+//        Set<GrantedAuthority> ga = new HashSet<>();
+//        for (Roles item : usuario.getRoles()){
+//            ga.add(new SimpleGrantedAuthority(item.getNombre()));
+//        }
+//        springUser = new org.springframework.security.core.userdetails.User(
+//                email,
+//                usuario.getPassword(),
+//                ga );
+//        return springUser;
+//
+//    }
 }
 
