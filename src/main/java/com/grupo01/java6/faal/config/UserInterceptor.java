6b88a71de1a30c85c5a3a9e5465ef06ee18fb8bd
Angeltorresrepo <angel.torrmoz@gmail.com>
2025-06-16 10:36:06 +0200

    Limpieza: ignorando archivos compilados y carpeta target

--- src/main/java/com/grupo01/java6/faal/config/UserInterceptor.java
+++ src/main/java/com/grupo01/java6/faal/config/UserInterceptor.java
@@ -2,6 +2,7 @@ package com.grupo01.java6.faal.config;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
